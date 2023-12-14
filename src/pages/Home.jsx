4850a56d811fe3b0734f11f400b040345593fcf7
Flávia Reis <flaviarster@gmail.com>
2023-12-14 20:17:49 -0300

    Treinando fetch e api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
+import { useEffect } from "react"
 import { useProductList } from "../services/useProductList"
 
 
 export const Home = () => {
-  const { products } = useProductList()
+  const { products, loadProducts } = useProductList()
+
+  useEffect(() => {
+    loadProducts()
+  }, [])
 
   return (
     <>
       <div>
           <h1>Home</h1>
-          <h2>This is the main page of the website</h2>
+          <h2>{products[0].product}</h2>
       </div>
 
     </>
--- src/services/useProductList.js
+++ src/services/useProductList.js
@@ -1,11 +1,19 @@
+import { useState } from "react"
 import { ApiUrl } from "../constants"
 
-export const useProductList = async () => {
-    try {
-        const response = await fetch(`${ApiUrl}/products`)
-        const data = await response.json()
-        console.log(data)
-    } catch (error) {
-        console.log(data)
+export const useProductList = () => {
+    const [products, setProducts] = useState()
+
+    const loadProducts = async () => {
+        try {
+            const response = await fetch(`${ApiUrl}/products`)
+            const data = await response.json()
+            console.log(1, data[0].product)
+            setProducts(data)
+        } catch (error) {
+            console.log(data)
+        }   
     }
+
+    return { products, loadProducts }
 }
